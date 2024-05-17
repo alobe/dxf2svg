c62754c7a421c2f7701042388c4d679ae85b842a
alobe <zhangwenbo@e-comm.com>
2024-05-17 13:53:05 +0800

    feat: add delete btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 import DxfParser, { IDxf } from 'dxf-parser'
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 import { D3Svg } from '@/components/d3Svg'
 import { Bg } from '@/components/Bg'
 import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { TrashCan } from '@/components/icons';
 
 export default function D3() {
   const p = new DxfParser()
   const [dxf, setDxf] = useState<IDxf>()
   const [fName, setFName] = useState('');
+  const ri = useRef<HTMLInputElement>(null)
   const [ref] = useAutoAnimate()
 
   const handleFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,8 +39,15 @@ export default function D3() {
       <div className="h-[60px] w-[60px] fixed top-3 left-3 overflow-hidden flex items-center justify-center rounded-full">
         <img src="/logo.webp" className="h-[60px] scale-[1.2] object-cover" alt="logo" />
       </div>
-      <Input className='w-[200px] flex-none cursor-pointer mt-6' type="file" onChange={handleFileInputChange} accept=".dxf"/>
-      {dxf && <D3Svg key={fName} dxf={dxf} />}
+      <div className="flex mt-6 flex-row items-center justify-center">
+        <Input ref={ri} className='w-[200px] flex-none cursor-pointer' type="file" onChange={handleFileInputChange} accept=".dxf"/>
+         {!!dxf && <TrashCan className='text-[40px] text-red-500 ml-3 hover:bg-red-400 hover:text-white bg-red-100 p-2 rounded cursor-pointer' onClick={() => (ri.current!.value = '', setDxf(undefined))} />}
+      </div>
+      {dxf ? <D3Svg key={fName} dxf={dxf} /> : (
+        <div className="text-white text-center mt-6 text-[20px] font-bold max-w-[50%] min-w-[300px]">
+          dxf2svg is a tool that helps you convert DXF file into SVG file. It also supports previewing the converted SVG file (such as zooming in and out, and dragging to move) and offers some basic editing features (such as adjusting line thickness and color, modifying the SVG viewBox setting, and controlling the visibility of certain elements from the DXF file).
+        </div>
+      )} 
     </div>
   )
 }
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -6,3 +6,9 @@ export function Reset(props: SVGProps<SVGSVGElement>) {
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...props}><path fill="currentColor" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z"></path></svg>
   )
 }
+
+export function TrashCan(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...props}><path fill="currentColor" d="M12 12h2v12h-2zm6 0h2v12h-2z"></path><path fill="currentColor" d="M4 6v2h2v20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8h2V6zm4 22V8h16v20zm4-26h8v2h-8z"></path></svg>
+  )
+}
